c623b37093228baa510e6286db8c8e37d595a17d
DESKTOP-6EVS0MU\Shmatkov Denis <shmatkov.d.i@gmail.com>
2016-10-01 20:12:14 +0300

    page products + link all page

--- js/main.js
+++ js/main.js
@@ -112,10 +112,27 @@ $(document).ready(function() {
 	  speed: 1000,
 	  slidesToShow: 1,
 	  adaptiveHeight: true,
-	  // autoplay: true,
-  	  // autoplaySpeed: 4000,
+	  autoplay: true,
+  	  autoplaySpeed: 4000,
 	});
 
+	 $('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+	});
+
+
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  centerMode: true,
+	  focusOnSelect: true
+	});
 
   
 
